66320c8b8232981b049a73e70d371eadbeb12460
Alessandro Resta <alessandro.resta@gmail.com>
2018-03-12 16:33:39 +0100

    [DEV-4122] Use reverse proxy to forward HTTP requests

--- proxy.go
+++ proxy.go
@@ -9,8 +9,10 @@ package main
 import (
 	"encoding/base64"
 	"io"
+	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"strings"
 	"time"
 
@@ -22,6 +24,7 @@ type Proxy struct {
 	Logger             *zap.Logger
 	AuthUser           string
 	AuthPass           string
+	ReverseProxy       *httputil.ReverseProxy
 	DestDialTimeout    time.Duration
 	DestReadTimeout    time.Duration
 	DestWriteTimeout   time.Duration
@@ -32,12 +35,6 @@ type Proxy struct {
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	p.Logger.Info("Incoming request", zap.String("host", r.Host))
 
-	if r.Method != http.MethodConnect {
-		p.Logger.Info("Method not allowed:", zap.String("method", r.Method))
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-
 	if p.AuthUser != "" && p.AuthPass != "" {
 		user, pass, ok := parseBasicProxyAuth(r.Header.Get("Proxy-Authenticate"))
 		if !ok || user != p.AuthUser || pass != p.AuthPass {
@@ -47,11 +44,26 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	p.connect(w, r)
+	if r.URL.Scheme == "http" {
+		p.handleHTTP(w, r)
+	} else {
+		p.handleTunneling(w, r)
+	}
 }
 
-func (p *Proxy) connect(w http.ResponseWriter, r *http.Request) {
-	p.Logger.Debug("Connecting:", zap.String("host", r.Host))
+func (p *Proxy) handleHTTP(w http.ResponseWriter, r *http.Request) {
+	p.Logger.Debug("Got HTTP request", zap.String("host", r.Host))
+	p.ReverseProxy.ServeHTTP(w, r)
+}
+
+func (p *Proxy) handleTunneling(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodConnect {
+		p.Logger.Info("Method not allowed", zap.String("method", r.Method))
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	p.Logger.Debug("Connecting", zap.String("host", r.Host))
 
 	destConn, err := net.DialTimeout("tcp", r.Host, p.DestDialTimeout)
 	if err != nil {
@@ -64,7 +76,7 @@ func (p *Proxy) connect(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 
-	p.Logger.Debug("Hijacking:", zap.String("host", r.Host))
+	p.Logger.Debug("Hijacking", zap.String("host", r.Host))
 
 	hijacker, ok := w.(http.Hijacker)
 	if !ok {
@@ -114,4 +126,22 @@ func parseBasicProxyAuth(auth string) (username, password string, ok bool) {
 		return
 	}
 	return cs[:s], cs[s+1:], true
+}
+
+// NewForwardingReverseProxy retuns a new reverse proxy that takes an incoming request and
+// sends it to another server, proxying the response back to the client.
+// See: https://golang.org/pkg/net/http/httputil/#ReverseProxy
+func NewForwardingReverseProxy(logger *log.Logger) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+	}
+	// TODO:(alesr) Use timeouts specified via flags to customize the default
+	// transport used by the reverse proxy.
+	return &httputil.ReverseProxy{
+		ErrorLog: logger,
+		Director: director,
+	}
 }
